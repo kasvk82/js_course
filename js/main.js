ea3d17dfc5bc883e5d83a453fc9f23772db75974
kasvk82 <kasvk82@gmail.com>
2025-04-06 15:24:05 +0300

    4 lesson

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// document.write("Java script говорит привет!");
+// console.log("Java script говорит привет!");
+// console.error("Java script говорит привет!");
+// console.warn("Java script говорит привет!");
+
+
+var num, str;
+num = 15; 
+console.log("Переменная: " + num);
