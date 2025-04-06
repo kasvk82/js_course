31fd7d714f6bd0840307727fb933fc2a7cb0d6a2
kasvk82 <kasvk82@gmail.com>
2025-04-06 17:46:20 +0300

    lesson6

--- js/main.js
+++ js/main.js
@@ -1,9 +1,60 @@
-// document.write("Java script говорит привет!");
-// console.log("Java script говорит привет!");
-// console.error("Java script говорит привет!");
-// console.warn("Java script говорит привет!");
+// document.write("Java script говорит привет!"); 
+console.log("Java script говорит привет!");
+console.error("Java script говорит привет!");
+console.warn("Java script говорит привет!");
 
 
 var num, str;
 num = 15; 
 console.log("Переменная: " + num);
+
+
+var num1 = 15;
+var num2 = 20;
+
+var num3 = num1 + num2;
+
+console.log("Сумма: " + num3);
+console.log("Сумма: " + (num + num1));
+
+val = [1,2,3,7,8];
+m_value = Math.max(num1, num2);
+console.log("Максимум: " + m_value);
+
+console.log("Массив: " + val[0]);
+
+console.log(val.length - 1);
+
+
+console.log(val);
+
+val[5] = 10;    
+
+console.log(val);
+
+val = 15;
+console.log(val);
+
+if (Number(val) > 4) {
+    console.log("хаха!")
+}
+else console.log("бубу");
+
+
+switch(num) {
+    case 1: 
+        console.log("бугага");
+        break;
+    case 15: 
+        console.log("брррр");
+        break;
+    case 20: 
+        console.log("фффрррр");
+        break;
+}
+
+
+
+
+
+
